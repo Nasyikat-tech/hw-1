cb2f2ec620c241b167b35278f91927c616d1b62f
Nasyikat Dzhabagieva <nasyikat.dzhabagieva.kk@gmail.com>
2022-05-19 17:19:18 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+//1.
+//let sms = "Nasyikat";
+//document.write(sms);
+//2.
+//let name = "Nasyikat";
+//document.write(name);
+//let name = "Aima";
+//document.write(name);
+//3.
+//let name = "Nasyikat";
+//let email = "nasyikat.dzhabagieva@gmail.com";
+//document.write(name, " " + email);
+//4.
+//let souvenir = prompt("Сколько сувенир вы купили?");
+//let bauble = prompt("Сколько безделушек вы купили?");
+//let result = souvenir * 75 + bauble * 112;
+//document.write(result);
+//5.
+/*let a = +prompt("Введите цифру");
+let b = +prompt("Введите цифру");
+let summ = a + b;
+let minus = a - b;
+let multiplication = a * b;
+let del = a / b;
+let ostDel = a % b;
+let vozv = a ** b;
+document.write(
+  "Сумма" +
+    " " +
+    summ +
+    " " +
+    "Разница" +
+    " " +
+    minus +
+    " " +
+    "Произведение" +
+    " " +
+    multiplication +
+    " " +
+    "Деления" +
+    " " +
+    del +
+    " " +
+    "Остаток от деления" +
+    " " +
+    ostDel +
+    " " +
+    "Возведения" +
+    " " +
+    vozv
+);*/
